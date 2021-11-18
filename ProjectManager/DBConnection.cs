9496cee0a030f475d4e136b33a0823871a9b6945
Anindra Das Bivas <bivasdas911@gmail.com>
2021-11-18 23:19:13 +0600

    Added some tables

--- ProjectManager/DBConnection.cs
+++ ProjectManager/DBConnection.cs
@@ -8,7 +8,7 @@ namespace ProjectManager
 {
     class DBConnection
     {
-        static string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""E:\Fall 21-22\OOP 2 Lab Task\Week10HomePrac\DB Files\ProjectManagerDB.mdf"";Integrated Security=True;Connect Timeout=30";
+        static string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""E:\Fall 21-22\OOP 2\Project\ProjectManagerApp\DB Files\ProjectManagerDB.mdf"";Integrated Security=True;Connect Timeout=30";
 
         public static string GetConnString()
         {
