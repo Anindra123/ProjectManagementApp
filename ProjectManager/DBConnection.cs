77873f88eadc519bfa24dc21130456ecfa2e91a1
Anindra Das Bivas <bivasdas911@gmail.com>
2021-11-18 19:18:25 +0600

    Added necessary class files

---
+++ ProjectManager/DBConnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager
+{
+    class DBConnection
+    {
+        static string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""E:\Fall 21-22\OOP 2 Lab Task\Week10HomePrac\DB Files\ProjectManagerDB.mdf"";Integrated Security=True;Connect Timeout=30";
+
+        public static string GetConnString()
+        {
+            return connectionString;
+        }
+    }
+}
---
+++ ProjectManager/ProjectGroup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager
+{
+    class ProjectGroup
+    {
+        public int PGroup_ID { get; set; }
+        public int MembersCount { get; set; }
+        public string PGroup_Name { get; set; }
+        public int Project_ID { get; set; }
+        public int PManager_ID { get; set; }
+
+    }
+}
