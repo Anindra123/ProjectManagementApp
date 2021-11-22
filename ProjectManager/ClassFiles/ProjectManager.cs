fc2ef1066a44ed9b572da27b5728037cdc36e3bc
Anindra Das Bivas <bivasdas911@gmail.com>
2021-11-22 17:52:04 +0600

    bulk namespace rename

--- ProjectManager/ClassFiles/ProjectManager.cs
+++ ProjectManager/ClassFiles/ProjectManager.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProjectManager.ClassFiles
+namespace ProjectManagement.ClassFiles
 {
     class ProjectManager : User
     {
--- ProjectManager/ClassFiles/Task.cs
+++ ProjectManager/ClassFiles/Task.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProjectManager.ClassFiles
+namespace ProjectManagement.ClassFiles
 {
     class Task
     {
